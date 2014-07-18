768f5f7f6b37161f93c31de584da09d57f3ca068
anupladha <anup.ladha@perceptivesoftware.com>
2014-07-18 11:29:00 +0000

    Our First Grid Working!

---
+++ App.js
@@ -0,0 +1,38 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    
+    launch: function() {
+    	console.log ('Our First App woot!');
+    	this._loadData();
+    },
+
+    //Get Data from Rally
+	_loadData: function() {
+		var myStore = Ext.create ('Rally.data.wsapi.Store', {
+            model: 'User Story',
+            autoLoad: true,
+            listeners: {
+                load: function (myStore, myData, success) {
+                    console.log ('got data', myStore, myData, success);
+                    this._loadGrid(myStore);
+                },
+        		scope: this
+            },
+            fetch: ['FormattedID', 'Name', 'ScheduleState']
+        });
+	},
+    
+    // Create & show a Grid of given stories
+    _loadGrid: function(myStoryStore) {
+        var myGrid = Ext.create('Rally.ui.grid.Grid', {
+        	store: myStoryStore,
+        	columnCfgs: [
+        	   'FormattedID', 'Name', 'ScheduleState'          
+        	]                    	
+        });
+        console.log ('my grid', myGrid);
+        this.add(myGrid);
+        console.log ('what is this?', this);
+    }   
+});
